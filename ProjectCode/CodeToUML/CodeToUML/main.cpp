0850ebd92817773877885ed21358af6228c07f34
Buwutiful <72912580+Buwutiful@users.noreply.github.com>
2022-10-13 15:52:51 -0500

    Add files via upload

---
+++ ProjectCode/CodeToUML/CodeToUML/main.cpp
@@ -0,0 +1,60 @@
+//Author: Adrian Cardona
+
+#include <iostream> //input/output
+#include <string> //the string itself
+//#include <filesystem> //To take in file path input
+
+inline void keep_window_open() {char ch; std::cin>>ch;}
+
+std::string promptForPath(std::string user_input);
+bool isValidPath(std::string user_input);
+
+int main() {
+	std::string file_path;
+
+	std::cout << "Welcome to the Code-to-UML Program." << std::endl;
+	promptForPath(file_path);
+
+	std::cout << "All done, press any button to exit. (Work in progress)" << std::endl;
+
+	keep_window_open();
+}
+
+std::string promptForPath(std::string user_input) {
+	bool run = true;
+
+	while(run) {
+		std::cout << "Please enter the file location of the code you'd like to produce a UML diagram for: " << std::endl;
+		std::cin >> user_input;
+
+		if(isValidPath(user_input) == true) {
+			run = false;
+		}
+	}
+
+	return user_input;
+}
+
+bool isValidPath(std::string user_input) {
+	std::string correctPath = "";
+	std::string temp = user_input;
+	bool run = true;
+	//std::filesystem::path path(temp);
+
+	while(run) {
+		std::cout << "Is '" << user_input << "' the correct path? (y/n)" << std::endl;
+		std::cin >> correctPath;
+
+		if(correctPath == "y") {
+			run = false;
+			return true;
+		}
+		else if(correctPath == "n") {
+			run = false;
+			return false;
+		}
+		else {
+			std::cout << "That is not the appropriate response." << std::endl;
+		}
+	}
+}
